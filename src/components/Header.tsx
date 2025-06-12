113fcd89c3b7bcb635e8cd6cc59d214a4e5fd052
D-Le-1 <dat28703@gmail.com>
2025-06-12 11:51:32 +0700

    fix screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="w-full bg-gray-800 p-4 text-white">
       <nav className="flex gap-4">
         <Link to="/">Trang chủ</Link>
         <Link to="/tim-kiem">Tìm kiếm</Link>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,9 +4,9 @@ import Footer from 'components/Footer'
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex min-h-screen w-full flex-col overflow-x-hidden">
       <Header />
-      <main className="flex-1 container mx-auto p-4">
+      <main className="mx-auto w-full flex-1 p-4">
         <Outlet />
       </main>
       <Footer />
